c06f78a334e918ba812cca911830fed9074fb512
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-07-02 11:59:55 +0530

    new changes

--- src/Components/Homecard.js
+++ src/Components/Homecard.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Homecard = ({ title, photos,address, price, loadingArray,id }) => {
   return (
-    <div className=" bg-slate-200 p-2 rounded-2xl shadow shadow-black w-full min-w-[300px] max-w-[300px] md:min-w-[500px] md:max-w-[500px] flex flex-col overflow-scroll scrollbar-none">
+    <div className=" bg-slate-200 p-2 rounded-2xl shadow shadow-black w-full min-w-[300px] max-w-[300px]  flex flex-col overflow-scroll scrollbar-none">
       {title ? (
         <>
         <Link to={ `/booking/${id}`} onClick={()=>window.scrollTo({top:"0",behavior : "smooth"})}>
-          <div className="w-60 min-h-[150px] rounded-2xl">
+          <div className="w-60 min-h-[180px] rounded-2xl">
             <img src={photos[0]} className="h-full w-full" />
           </div>
           <h2 className='text-lg font-serif font-bold text-blue-400'>{title}</h2>
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -15,14 +15,17 @@ const DataProduct = useSelector((state)=>state.product.productList)
   
   return (
     <div className='w-full'>
-           <div className='w-full h-[300px]'><img src={banner} className='w-full h-full' alt=''/></div>  
+           <div className='w-full h-[400px]'><img src={banner} className='w-full h-full' alt=''/></div>  
 
-        <div className='md:flex grid grid-cols-2 gap-4  py-3 overflow-scroll scrollbar-none '>
+        <div className='md:flex grid md:grid-cols-2 grid-cols-1  gap-4  py-3 overflow-scroll scrollbar-none '>
          
-          <div className='w-1/2 p-4'>
-            <div className='shadow w-23 h-20'><Link to={"login"}><img src={book} alt='' className='h-full w-full'/></Link></div>
-            <p className="text-black my-4 shadow rounded-2xl border-t border-primary font-bold text-2xl">Register to see more cheap Apartments!!! </p>
+          <div className='w-1/2 p-4 '>
+            <div className='min-w-[300px] max-w-[300px] bg-slate-200 p-2 rounded-2xl shadow shadow-black flex flex-col overflow-scroll scrollbar-none'>
+            <div className='w-60 min-h-[180px]'><Link to={"login"}><img src={book} alt='' className='h-full w-full'/></Link></div>
+            <p className="text-black my-4 shadow rounded-2xl border-t items-center border-primary font-bold text-2xl">Register <span>For More Apartments!!!</span> </p>
             <div className='mt-4'><Link to={"login"} className='w-[150px] flex p-2 items-center rounded-full shadow bg-primary'>View Apartments</Link></div>
+            </div>
+           
           </div>
 
           <div className='w-1/2 flex flex-wrap gap-y-7 gap-6 p-4 justify-center'>
