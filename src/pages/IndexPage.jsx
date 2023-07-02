19720dfcbbf4b56350f94d2b91e454bb2c09b823
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-07-02 13:32:40 +0530

    apartments

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -22,7 +22,7 @@ const DataProduct = useSelector((state)=>state.product.productList)
          
           <div className='md:w-1/2 md:p-4  '>
             <div className='min-w-[300px] max-w-[300px] bg-slate-200 p-2 rounded-2xl shadow shadow-black flex flex-col overflow-scroll scrollbar-none'>
-            <div className='w-60 min-h-[180px] px-2'><Link to={"login"}><img src={book} alt='' className='h-full w-full'/></Link></div>
+            <div className='w-[270px] min-h-[210px] px-2'><Link to={"login"}><img src={book} alt='' className='h-full w-full'/></Link></div>
             <p className="text-black my-4 shadow rounded-2xl border-t items-center border-primary font-bold text-2xl">Register <span>For More Apartments!!!</span> </p>
             <div className='mt-4'><Link to={"login"} className='w-[150px] flex p-2 items-center rounded-full shadow bg-primary'>View Apartments</Link></div>
             </div>
