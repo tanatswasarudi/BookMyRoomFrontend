3ece9482c94b04f315d8f7466fdca7c806b141a4
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-07-03 14:37:51 +0530

    fixed lawgate sign

--- src/Components/FilterProducts.js
+++ src/Components/FilterProducts.js
@@ -4,7 +4,7 @@ import {MdBedroomParent} from 'react-icons/md'
 const FilterProduct = ({category,onClick}) => {
   return (
     <div onClick={onClick} className=''>
-        <div className='md:text-7xl  md:p-5 bg-primary rounded-full cursor-pointer hover:to-red-300'>
+        <div className='text-7xl  p-5 bg-primary rounded-full cursor-pointer hover:to-red-300'>
              <MdBedroomParent className=''/>
            </div>
            <p className='text-center font-medium my-1 capitalize'>{category}</p>
